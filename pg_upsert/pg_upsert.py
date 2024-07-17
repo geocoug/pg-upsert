049b7a5e374ddccc360c5c7d4c0651c214cf7ac1
Caleb Grant <geocoug@gmail.com>
2024-07-17 16:08:47 -0700

    Bump version: 1.1.1 → 1.1.2

--- pg_upsert/pg_upsert.py
+++ pg_upsert/pg_upsert.py
@@ -19,7 +19,7 @@ from psycopg2.extras import DictCursor
 from psycopg2.sql import SQL, Composable, Identifier, Literal
 from tabulate import tabulate
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 
 description_long = """
 Check data in a staging table or set of staging tables, then update and insert (upsert)
