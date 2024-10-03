a514faf867c4d3c3a9b0be4abab6b81605328c55
Caleb Grant <geocoug@gmail.com>
2024-10-03 09:38:12 -0700

    + publisher table to test for null values in column with FK constraint
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- tests/data.sql
+++ tests/data.sql
@@ -4,13 +4,25 @@ create table public.genres (
     description varchar not null
 );
 
+drop table if exists public.publishers cascade;
+create table public.publishers (
+    publisher_id varchar(100) primary key,
+    publisher_name varchar(200) null
+);
+
 drop table if exists public.books cascade;
 create table public.books (
     book_id varchar(100) primary key,
     book_title varchar(200) not null,
     genre varchar(100) not null,
+    publisher_id varchar(100) null,
     notes text,
     foreign key (genre) references public.genres(genre)
+        on update cascade
+        on delete restrict,
+    foreign key (publisher_id) references public.publishers(publisher_id)
+        on update cascade
+        on delete restrict
 );
 
 drop table if exists public.authors cascade;
@@ -27,8 +39,12 @@ drop table if exists public.book_authors cascade;
 create table public.book_authors (
     book_id varchar(100) not null,
     author_id varchar(100) not null,
-    foreign key (author_id) references public.authors(author_id),
-    foreign key (book_id) references public.books(book_id),
+    foreign key (author_id) references public.authors(author_id)
+        on update cascade
+        on delete restrict,
+    foreign key (book_id) references public.books(book_id)
+        on update cascade
+        on delete restrict,
     constraint pk_book_authors primary key (book_id, author_id)
 );
 
@@ -42,11 +58,18 @@ create table staging.genres (
     description varchar
 );
 
+drop table if exists staging.publishers cascade;
+create table staging.publishers (
+    publisher_id varchar(100),
+    publisher_name varchar(200)
+);
+
 drop table if exists staging.books cascade;
 create table staging.books (
     book_id varchar(100),
     book_title varchar(200),
     genre varchar(100),
+    publisher_id varchar(100),
     notes text
 );
 
@@ -68,14 +91,18 @@ insert into staging.genres (genre, description) values
     ('Fiction', 'Literary works that are imaginary, not based on real events or people'),
     ('Non-Fiction', 'Literary works based on real events, people, and facts');
 
+insert into staging.publishers (publisher_id, publisher_name) values
+    ('P001', 'Publisher 1'),
+    ('P002', null);
+
 insert into staging.authors (author_id, first_name, last_name) values
     ('JDoe', 'John', 'Doe'),
     ('JSmith', 'Jane', 'Smith'),
     ('JTrent', 'Joe', 'Trent');
 
-insert into staging.books (book_id, book_title, genre, notes) values
-    ('B001', 'The Great Novel', 'Fiction', 'An epic tale of love and loss'),
-    ('B002', 'Not Another Great Novel', 'Non-Fiction', 'A comprehensive guide to writing a great novel');
+insert into staging.books (book_id, book_title, genre, publisher_id, notes) values
+    ('B001', 'The Great Novel', 'Fiction', 'P001', 'An epic tale of love and loss'),
+    ('B002', 'Not Another Great Novel', 'Non-Fiction', null, 'A comprehensive guide to writing a great novel');
 
 insert into staging.book_authors (book_id, author_id) values
     ('B001', 'JDoe'),
