7f0af957cf2a3a5b3acfb6d8ae6f0b5f77525d3f
Caleb Grant <geocoug@gmail.com>
2024-11-13 07:40:04 -0800

    Bump version: 1.4.1 → 1.4.2

--- pg_upsert/__version__.py
+++ pg_upsert/__version__.py
@@ -4,5 +4,5 @@ __code_url__ = "https://github.com/geocoug/pg_upsert"
 __docs_url__ = "https://pg-upsert.readthedocs.io/"
 __author_email__ = "grantcaleb22@gmail.com"
 __license__ = "GNU GPLv3"
-__version__ = "1.4.1"
+__version__ = "1.4.2"
 __description__ = "Run not-NULL, Primary Key, Foreign Key, and Check Constraint checks on staging tables then update and insert (upsert) data from staging tables to base tables."  # noqa: E501
