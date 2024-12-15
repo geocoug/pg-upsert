13041d58b75aaf52c49d3d8821e98b27aa19f727
Caleb Grant <geocoug@gmail.com>
2024-12-15 13:50:42 -0800

    apply custom debug log formatting
    Signed-off-by: Caleb Grant <geocoug@gmail.com>

--- pg_upsert/cli.py
+++ pg_upsert/cli.py
@@ -12,9 +12,9 @@ import yaml
 
 from .__version__ import __description__, __docs_url__, __title__, __version__
 from .upsert import PgUpsert
+from .utils import CustomLogFormatter
 
 logger = logging.getLogger(__title__)
-logger.setLevel(logging.INFO)
 logger.propagate = False
 
 
@@ -60,14 +60,14 @@ def clparser() -> argparse.Namespace:
     parser.add_argument(
         "-e",
         "--exclude-columns",
-        dest="exclude",
+        dest="exclude_columns",
         type=str,
         help="comma-separated list of columns to exclude from null checks",
     )
     parser.add_argument(
         "-n",
         "--null-columns",
-        dest="null",
+        dest="null_columns",
         type=str,
         help="comma-separated list of columns to exclude from null checks",
     )
@@ -86,6 +86,7 @@ def clparser() -> argparse.Namespace:
     parser.add_argument(
         "-m",
         "--upsert-method",
+        dest="upsert_method",
         default="upsert",
         choices=["upsert", "update", "insert"],
         help="method to use for upsert",
@@ -93,46 +94,40 @@ def clparser() -> argparse.Namespace:
     parser.add_argument(
         "-h",
         "--host",
-        # required=True,
         type=str,
         help="database host",
     )
     parser.add_argument(
         "-p",
         "--port",
-        # required=True,
         type=int,
         help="database port",
     )
     parser.add_argument(
         "-d",
         "--database",
-        # required=True,
         type=str,
         help="database name",
     )
     parser.add_argument(
         "-u",
         "--user",
-        # required=True,
         type=str,
         help="database user",
     )
     parser.add_argument(
         "-s",
         "--staging-schema",
+        dest="staging_schema",
         default="staging",
-        dest="stg_schema",
-        # required=True,
         type=str,
         help="staging schema name",
     )
     parser.add_argument(
         "-b",
         "--base-schema",
-        default="public",
         dest="base_schema",
-        # required=True,
+        default="public",
         type=str,
         help="base schema name",
     )
@@ -153,7 +148,7 @@ def clparser() -> argparse.Namespace:
     )
     parser.add_argument(
         "-t",
-        "--table",
+        "--tables",
         nargs="+",
         default=[],
         help="table name(s)",
@@ -189,7 +184,9 @@ def main() -> None:
                 else:
                     setattr(args, key, config[key])
             else:
-                logger.warning(f"Invalid configuration key will be ignored: {key}")
+                logger.warning(
+                    f"Invalid configuration key will be ignored in {args.config_file}: {key}",
+                )
     if not args.host:
         logger.error("Database host is required.")
         sys.exit(1)
@@ -199,50 +196,72 @@ def main() -> None:
     if not args.user:
         logger.error("Database user is required.")
         sys.exit(1)
-    if not args.stg_schema:
+    if not args.staging_schema:
         logger.error("Staging schema is required.")
         sys.exit(1)
     if not args.base_schema:
         logger.error("Base schema is required.")
         sys.exit(1)
-    if not args.table:
+    if not args.tables:
         logger.error("One or more table names are required.")
         sys.exit(1)
     if args.logfile and args.logfile.exists():
-        args.logfile.unlink()
-    if not args.quiet:
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.INFO)
-        stream_handler.setFormatter(logging.Formatter("%(message)s"))
-        logger.addHandler(stream_handler)
-    if args.logfile:
-        file_handler = logging.FileHandler(args.logfile)
-        file_handler.setLevel(logging.INFO)
-        logger.addHandler(file_handler)
+        try:
+            args.logfile.unlink()
+        except Exception as err:
+            logger.error(err)
     if args.debug:
         logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(lineno)d: %(message)s",
-        )
-        for handler in logger.handlers:
-            handler.setFormatter(formatter)
-            handler.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    if args.logfile:
+        file_handler = logging.FileHandler(args.logfile)
+        if args.debug:
+            file_handler.setLevel(logging.DEBUG)
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "[%(asctime)s] %(levelname)-8s %(name)-20s %(lineno)-5s: %(message)s",
+                ),
+            )
+        else:
+            file_handler.setLevel(logging.INFO)
+            file_handler.setFormatter(logging.Formatter("[%(asctime)s]: %(message)s"))
+        logger.addHandler(file_handler)
+    if not args.quiet:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        if args.debug:
+            stream_handler.setLevel(logging.DEBUG)
+            stream_handler.setFormatter(
+                CustomLogFormatter(
+                    "[%(asctime)s] %(levelname)s %(name)s %(lineno)s: %(message)s",
+                ),
+            )
+        else:
+            stream_handler.setLevel(logging.INFO)
+            stream_handler.setFormatter(logging.Formatter("%(message)s"))
+        logger.addHandler(stream_handler)
+    logger.debug(f"{__title__} (v{__version__})")
+    if args.debug:
         logger.debug("Command line arguments:")
         for arg in vars(args):
             padding = " " * (max(map(len, vars(args))) - len(arg))
             logger.debug(f"{arg}:{padding} {getattr(args, arg)}")
+    if args.exclude_columns and isinstance(args.exclude_columns, str):
+        args.exclude_columns = args.exclude_columns.split(",")
+    if args.null_columns and isinstance(args.null_columns, str):
+        args.null_columns = args.null_columns.split(",")
     try:
         PgUpsert(
             uri=f"postgresql://{args.user}@{args.host}:{args.port}/{args.database}",
             encoding="utf-8",
-            tables=args.table,
-            stg_schema=args.stg_schema,
+            tables=args.tables,
+            stg_schema=args.staging_schema,
             base_schema=args.base_schema,
             do_commit=args.commit,
             upsert_method=args.upsert_method,
             interactive=args.interactive,
-            exclude_cols=args.exclude.split(",") if args.exclude else None,
-            exclude_null_check_cols=args.null.split(",") if args.null else None,
+            exclude_cols=args.exclude_columns,
+            exclude_null_check_cols=args.null_columns,
         ).run()
     except Exception as e:
         logger.error(e)
--- pg_upsert/utils.py
+++ pg_upsert/utils.py
@@ -2,9 +2,35 @@
 
 from __future__ import annotations
 
+import logging
 from datetime import datetime
+from typing import ClassVar
 
-from .__version__ import __description__, __version__
+
+class CustomLogFormatter(logging.Formatter):
+    """
+    Custom logging formatter that adds color and padding to the log messages.
+    """
+
+    COLORS: ClassVar[dict[str, str]] = {
+        "DEBUG": "\033[94m",  # Blue
+        "INFO": "\033[92m",  # Green
+        "WARNING": "\033[93m",  # Yellow
+        "ERROR": "\033[91m",  # Red
+        "CRITICAL": "\033[41m",  # Red background
+        "RESET": "\033[0m",  # Reset to default
+    }
+
+    def format(self: CustomLogFormatter, record: logging.LogRecord) -> str:
+        self.datefmt = "%Y-%m-%d %H:%M:%S"
+        self.asctime = self.formatTime(record, self.datefmt)
+        record.name = f"{record.name:<20}"
+        record.lineno = f"{record.lineno!s:<5}"
+        # Get the color corresponding to the log level
+        color = self.COLORS.get(record.levelname, self.COLORS["RESET"])
+        # Apply color and padding to the log level name
+        record.levelname = f"{color}{record.levelname:<8}{self.COLORS['RESET']}"
+        return super().format(record)
 
 
 def ellapsed_time(start_time: datetime) -> str:
